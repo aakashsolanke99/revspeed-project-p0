7dc99685e4f14a25976f283e8bc80991664524a9
aakashsolanke99 <aakashsolanke99@gmail.com>
2023-12-24 19:10:30 +0530

    deleted project

--- revspeed-p0/src/main/java/com/revature/service/BroadbandServicesService.java
+++
@@ -1,9 +0,0 @@
-package com.revature.service;
-
-import com.revature.utile.BroadBandServicePlans;
-
-import java.util.List;
-
-public interface BroadbandServicesService {
-    public List<BroadBandServicePlans> getAllBroadbandServicePlans();
-}
--- revspeed-p0/src/main/java/com/revature/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.revature.service;
-
-import java.sql.SQLException;
-
-public interface UserService {
-    public void register() throws SQLException;
-    public void login() throws SQLException;
-}
